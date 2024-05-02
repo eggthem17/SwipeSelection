dcaa57b3ab1f05e64888956a81be0a2f715cd8ad
eggthem17 <paxmiler@gmail.com>
2024-05-02 20:20:32 +0900

    Initial Commit

---
+++ SwipeSelection/SwipeSelectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwipeSelectionApp.swift
+//  SwipeSelection
+//
+//  Created by MinKyu CHA on 5/2/24.
+//
+
+import SwiftUI
+
+@main
+struct SwipeSelectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
